e7527c9e248a74cff8161f422a840b130534af06
Abir Haque <abirhaque@outlook.com>
2020-10-02 15:04:03 -0400

    adding Flix - Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Abir Haque on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
